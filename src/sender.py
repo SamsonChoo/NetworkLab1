65fa72b05111f1d24213a236ab888186df092454
SamsonChoo <hanye_choo@mymail.sutd.edu.sg>
2018-10-24 20:22:26 +0800

    corrected sender.py

--- src/sender.py
+++ src/sender.py
@@ -25,7 +25,7 @@ def main():
     # Define customized header (Task 3.)
     my_id = '0745079'
     my_dept = 'cs'
-    my_gender = 'male'
+    my_gender = 1
     student = Protocol(id = my_id, dept = my_dept, gender = my_gender)
 
     # Read file and store into list
